612004d755cf177acf1bf3e416f42eebc48a4787
Vladimir Stolyarevsky <saoromant@gmail.com>
2017-02-08 14:14:11 +0200

    fixed errors in m and t calc

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-"use strict";
-
+"use strict"; 
 
 function  calcJaroWincklerDistance(str1,str2){
 
-    var s1 = str1.length;
-    var s2 = str2.length;
+    var str1Length = str1.length;
+    var str2Length = str2.length;
 
-    var range = Math.floor(Math.max(s1,s2)/2) - 1;
+    var range = Math.floor(Math.max(str1Length,str2Length)/2) - 1;
     var m = 0;
     var t = 0;
     var l = 0;
     var isLSet = false;
     var lastMatchJ = 0;
-    for(var i = 1;i<=s1;i++){
-        var c1 = str1[i-1];
-        for(var j =1;j<=s2;j++){
+    for(var i = 0;i<str1Length;i++){
+        var c1 = str1[i];
+        for(var j =i;j<str2Length;j++){
             if(Math.abs(i-j) > range)
                 continue;
-            var c2 = str2[j-1];
+            var c2 = str2[j];
             if(c1==c2) {
-
-                m++;
+                m++; //characters is the same and within range               
                 if (i != j) {
                     if (lastMatchJ > j)
                         t += 2;
                 }
                 else {
                     if (!isLSet && l < 4) {
-
                         l++;
                     }
                 }
                 lastMatchJ = j;
+                break;
             }else {
                 if(i==j)
                     isLSet =true;
@@ -39,15 +37,14 @@ function  calcJaroWincklerDistance(str1,str2){
         }
     }
     t =  0.5*t;
-    m = Math.min(m,s1,s2);
-    //  console.log('m='+m,' t='+t,' s1='+s1,' s2='+s2,' r='+range,' l='+l);
+    m = Math.min(m,str1Length,str2Length);
+    console.log('m='+m,' t='+t,' s1='+str1Length,' s2='+str2Length,' r='+range,' l='+l);
     var dj = 0;
     if(m>0)
-        dj = (m/s1 + m/s2 + (m-t)/m)/3;
-
+        dj = (m/str1Length + m/str2Length + (m-t)/m)/3;
+//console.log('m='+m,' t='+t,' s1='+str1Length,' s2='+str2Length,' r='+range,' l='+l,' dj='+dj);
 
     return dj + (l*0.1*(1-dj));
-}
-
+} 
 module.exports = calcJaroWincklerDistance;
 
